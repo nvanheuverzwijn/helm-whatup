2da7796783f75b5c47c8c12914f558b1c8fba0b6
Emanuel Bennici <eb@fabmation.de>
2020-01-21 18:18:18 +0100

    Correct variable name spelling misstake

--- main.go
+++ main.go
@@ -100,7 +100,7 @@ func newOutdatedCmd(cfg *action.Configuration, out io.Writer) *cobra.Command {
 	}
 
 	flags := cmd.Flags()
-	flags.BoolVar(&deprecatedFlags, "deprecation-notice", true, "disable it to prevent printing the deprecation notice message")
+	flags.BoolVar(&deprecationInfo, "deprecation-notice", true, "disable it to prevent printing the deprecation notice message")
 	flags.BoolVar(&ignoreNoRepo, "ignore-repo", false, "ignore error if no repo for a chart is found")
 	flags.Bool("devel", false, "use development versions (alpha, beta, and release candidate releases), too. Equivalent to version '>0.0.0-0'.")
 	flags.BoolVarP(&client.Short, "short", "q", false, "output short (quiet) listing format")
